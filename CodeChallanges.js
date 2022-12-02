cd72dffbbc825061c25829852490aff9162101a6
Chandler Dearden <chandlerjdearden@gmail.com>
2022-12-02 15:57:44 -0700

    Computer Science Assessment

--- CodeChallanges.js
+++ CodeChallanges.js
@@ -1,16 +1,17 @@
-let array = [28, 43, -12, 30, 4, 0, 12]
-let value = false;
+function addToZero(array) {
 for (let i = 0; i < array.length; i++) {
   for (let j = 0; j < array.length; j++) {
     if (i !== j) {
       if (array[i] + array[j] === 0) {
-        value = true;
+         return true
       } 
     }
   }
 }
+return false
+}
 
-console.log(value)
+console.log(addToZero([28, 43, -12, 30, 4, 0, 12]))
 // Run time of O(n^2) //
 
 function hasUniqueChars(word) {
